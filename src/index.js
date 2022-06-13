426405d747e5bfa39a099cd8f2d5af053ba0cfad
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-13 22:02:20 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const list = document.getElementById("tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newTask = document.getElementById("new-task-description");
+    createListItem(newTask.value);
+    form.reset();
+  })
+
+  function createListItem(listItem) {
+    const newListItem = document.createElement("li");
+    newListItem.textContent = `${listItem} `;
+
+    const newButton = document.createElement("button");
+    newButton.addEventListener("click", handleDelete)
+    newButton.textContent = "X";
+    newListItem.appendChild(newButton);
+
+    list.append(newListItem);
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove();
+  }
 });
